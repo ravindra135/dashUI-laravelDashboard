97b0041d2e23b87d69534763360e0c61047a3228
Ravindra Reddy <reddyravindra947@gmail.com>
2022-09-08 13:11:54 +0530

    made changes in login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,6 +11,11 @@
                             <div class="card-header pb-0 text-left bg-transparent">
                                 <h3 class="font-weight-bolder text-info text-gradient">Welcome back</h3>
                                 <p class="mb-0">Enter your email and password to sign in</p>
+                                <p class="mb-0">
+                                    SuperAdmin Demo:<br>
+                                    Email: superadmin@dashui.dev<br>
+                                    pass:  password
+                                </p>
                             </div>
                             <div class="card-body">
                                 <form method="POST" action="{{ route('login') }}" role="form">
